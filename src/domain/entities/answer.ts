c0893ad8de4782b3da11dc3aadfadd899cd92ce0
Mateus Macedo <mateusmacedoeng@gmail.com>
2026-05-14 15:41:33 -0300

    add mapping properties

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,12 @@
 import { Entity } from "../../core/entities/entity.js"
+import type { UniqueEntityID } from "../../core/entities/unique-entity-id.js"
 
 interface AnswerProps {
+    authorId: UniqueEntityID, 
+    questionId: UniqueEntityID
     content: string, 
-    authorId: string, 
-    questionId: string
+    createdAt: Date,
+    updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import type { Slug } from "./value-objects/slug.js"
 import { Entity } from "../../core/entities/entity.js"
+import type { UniqueEntityID } from "../../core/entities/unique-entity-id.js"
 
 interface QuestionProps {
+    authorId: UniqueEntityID,
+    bestAnswerId?: UniqueEntityID
     title: string, 
     content: string,
-    slug: Slug
-    authorId: string, 
+    slug: Slug,
+    createdAt: Date,
+    updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {}
