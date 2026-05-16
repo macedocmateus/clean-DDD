c31e740120f84332b4c138c8019ddaa05aaff1a1
Mateus Macedo <mateusmacedoeng@gmail.com>
2026-05-16 16:36:29 -0300

    add get question find by slug use-case

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -3,6 +3,16 @@ import type { Question } from "@/domain/forum/enterprise/entities/question.js";
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
     public items: Question[] = []
+
+    async findBySlug(slug: string) {
+        const question = this.items.find((item) => item.slug.value === slug)
+
+        if (!question) {
+            return null
+        }
+
+        return question
+    }
     
     async create(question: Question) {
         this.items.push(question)
